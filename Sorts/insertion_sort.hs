e8f0795865943e8ce9e44bdb90ca6246bc15f1ca
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2021-02-18 18:08:36 +0700

    🐛 Fix logic error in Haskell insertion_sort

--- Sorts/insertion_sort.hs
+++ Sorts/insertion_sort.hs
@@ -4,4 +4,4 @@ insertionSort (x : xs) = insert x (insertionSort xs)
   where
     insert x [] = [x]
     insert x xs@(y : ys) | x < y     = x : xs
-                         | otherwise = y : insert x xs
+                         | otherwise = y : insert x ys
